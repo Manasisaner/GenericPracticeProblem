389b6f3af50f64b90e942b6ca8469ddab113dcdf
snehasonwane <snehasonwane01@gmail.com>
2022-05-06 22:44:15 +0530

    Generc statement

---
+++ Genericprojectstatement/PrintingArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProjectStatement
+{
+    public class PrintingArrayElement
+    {
+        public void Maximumelement<T>(T[] ArrayElement)
+        {
+            foreach (T element in ArrayElement)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine(" ");
+            Array.Sort(ArrayElement);
+            int lent = ArrayElement.Length;
+            Console.WriteLine("Greatest Element is:->" + ArrayElement[lent - 1]);
+        }
+    }
+}
---
+++ Genericprojectstatement/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+using GenericsProjectStatement;
+class Program
+{
+    public static void Main(String[] args)
+    {
+        PrintingArrayElement arr = new PrintingArrayElement();
+        int[] intArray = { 1, 2, 4, 58, 7, 5, 54, 55489, 5894 };
+        float[] floatArray = { 10.5f, 15.2f, 0.0005f, -1.25f };
+        string[] stringArray = { "abc", "abcd", "abcde", "abcef" };
+        arr.Maximumelement<int>(intArray);
+        arr.Maximumelement<float>(floatArray);
+        arr.Maximumelement<string>(stringArray);
+    }
+}
