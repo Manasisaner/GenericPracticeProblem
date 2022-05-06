dae0bd95f126a5ea3667891fba8a32ff196a5c31
snehasonwane <snehasonwane01@gmail.com>
2022-05-06 22:01:01 +0530

    Test Maximum

---
+++ FindMaximumnumber/Testmaimum/Unittest1.cs
@@ -0,0 +1,18 @@
+﻿using FindMaximum;
+using NUnit.Framework;
+
+
+namespace TestMaximums
+{
+    public class Tests
+    {
+        [Test]
+        public void GivenStringInput_WhenTestMax_FirstPosition_ShouldReturnMaxNum()
+        {
+            MaxString max = new MaxString();
+            string actual = max.FindMaxString("Banana", "Peach", "Apple");
+            Assert.AreEqual(actual, "Banana");
+        }
+
+    }
+}
---
+++ FindMaximumnumber/Testmaimum/Unittest2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FindMaximum;
+using NUnit.Framework;
+
+namespace TestMaximums
+{
+    public class UnitTest1
+    {
+        public void GivenStringInput_WhenTestMax_SecondPosition_ShouldReturnMaxNum()
+        {
+            MaxString max = new MaxString();
+            string actual = max.FindMaxString("Peach", "Banana", "Apple");
+            Assert.AreEqual(actual, "Banana");
+        }
+    }
+}
